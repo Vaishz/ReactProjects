eea9f2aed05c6cb69a34b5bb518558dad5035085
yepvaishz <poti.vaishnavi21@gmail.com>
2024-02-19 21:13:20 +0530

    changes to vaish

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import Vaish from "./Vaish";
 
 
 
@@ -13,6 +14,7 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/vaish" element={<Vaish />} />
         </Routes>
       </BrowserRouter> 
       <Footer />
